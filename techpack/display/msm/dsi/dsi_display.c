cf3ab8b1ffd5e338b5f20066df28564da12d0574
nullptr03 <nullptr03@singkolab.my.id>
2025-01-09 16:20:47 +0000

    drm: Add hbm sysfs node BSP.LCM - 2022.07.15 - modify for LCM add hbm

--- techpack/display/msm/dsi/dsi_display.c
+++ techpack/display/msm/dsi/dsi_display.c
@@ -26,6 +26,7 @@
 #include "sde_expo_dim_layer.h"
 #endif
 
+#define to_dsi_bridge(x) container_of((x), struct dsi_bridge, base)
 #define to_dsi_display(x) container_of(x, struct dsi_display, host)
 #define INT_BASE_10 10
 
@@ -8874,6 +8875,53 @@ int dsi_display_unprepare(struct dsi_display *display)
 
 struct dsi_display *get_main_display(void) {
 	return primary_display;
+}
+
+ssize_t dsi_display_set_hbm(struct drm_connector *connector, int hbm_status)
+{
+	ssize_t rc;
+	struct dsi_display *display = NULL;
+	struct dsi_bridge *c_bridge = NULL;
+
+	if (!connector || !connector->encoder || !connector->encoder->bridge) {
+		pr_err("Invalid connector/encoder/bridge ptr\n");
+		return -EINVAL;
+	}
+
+	c_bridge =  to_dsi_bridge(connector->encoder->bridge);
+	display = c_bridge->display;
+
+	if (!display || !display->panel) {
+		pr_err("Invalid display/panel ptr\n");
+		return -EINVAL;
+	}
+
+	rc = dsi_panel_set_hbm(display->panel, hbm_status);
+
+	return rc;
+}
+
+ssize_t dsi_display_get_hbm_status(struct drm_connector *connector)
+{
+	ssize_t hbm_status;
+	struct dsi_display *display = NULL;
+	struct dsi_bridge *c_bridge = NULL;
+
+	if (!connector || !connector->encoder || !connector->encoder->bridge) {
+		pr_err("Invalid connector/encoder/bridge ptr\n");
+		return -EINVAL;
+	}
+
+	c_bridge =  to_dsi_bridge(connector->encoder->bridge);
+	display = c_bridge->display;
+
+	if (!display || !display->panel) {
+		pr_err("Invalid display/panel ptr\n");
+		return -EINVAL;
+	}
+	hbm_status = display->panel->bl_config.hbm_status;
+
+	return hbm_status;
 }
 
 void __init dsi_display_register(void)
