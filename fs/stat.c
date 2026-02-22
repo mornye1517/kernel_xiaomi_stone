a0c138999580efe33e452f24757630852910c989
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-02-22 12:56:11 +0000

    fs: Update ksu manual hooks to v1.9
    changes of this commits:
    	fs/read_write: switch to ksu_init_rc_hook hook
    	fs/stat: sys_newfstatat: ksu_handle_stat hook
    	fs/stat: sys_fstatat64: ksu_handle_stat hook
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- fs/stat.c
+++ fs/stat.c
@@ -361,6 +361,8 @@ SYSCALL_DEFINE2(newlstat, const char __user *, filename,
 __attribute__((hot)) 
 extern int ksu_handle_stat(int *dfd, const char __user **filename_user,
 				int *flags);
+extern void ksu_handle_newfstat_ret(unsigned int *fd, struct stat __user **statbuf_ptr);
+extern void ksu_handle_fstat64_ret(unsigned long *fd, struct stat64 __user **statbuf_ptr);
 #endif
 
 #if !defined(__ARCH_WANT_STAT64) || defined(__ARCH_WANT_SYS_NEWFSTATAT)
@@ -388,6 +390,9 @@ SYSCALL_DEFINE2(newfstat, unsigned int, fd, struct stat __user *, statbuf)
 	if (!error)
 		error = cp_new_stat(&stat, statbuf);
 
+#ifdef CONFIG_KSU
+	ksu_handle_newfstat_ret(&fd, &statbuf);
+#endif
 	return error;
 }
 #endif
@@ -514,6 +519,10 @@ SYSCALL_DEFINE2(fstat64, unsigned long, fd, struct stat64 __user *, statbuf)
 
 	if (!error)
 		error = cp_new_stat64(&stat, statbuf);
+
+#ifdef CONFIG_KSU_MANUAL_HOOK // for 32-bit
+	ksu_handle_fstat64_ret(&fd, &statbuf);
+#endif
 
 	return error;
 }
